13524970a00ab3d74d8425f1abdab430f997a981
Allen <Allen@DESKTOP-2IQMIJL>
2016-07-20 13:06:15 -0300

    Release

---
+++ src/DimDim.Model/Despesa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimDim.Model
+{
+    public class Despesa
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Data { get; set; }
+        public decimal Valor { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ src/DimDim.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DimDim.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimDim.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new HomeViewModel
+            {
+                Now = DateTimeOffset.Now,
+                Title = "Dim Dim"
+            };
+            return View(vm);
+        }
+    }
+}
