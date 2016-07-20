caeca9e67601cc508e1e54fe5b8cccc078e4acd5
Allen <Allen@DESKTOP-2IQMIJL>
2016-07-20 14:38:04 -0300

    Despesa Controller e Razor Tools para Intelissense

---
+++ src/DimDim.Web/Controllers/DespesasController.cs
@@ -0,0 +1,23 @@
+﻿using DimDim.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimDim.Web.Controllers
+{
+    public class DespesasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            var vm = new RegistroDespesaViewModel();
+            return View(vm);
+        }
+    }
+}
---
+++ src/DimDim.Web/ViewModels/RegistrarDespesaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimDim.Web.ViewModels
+{
+    public class RegistroDespesaViewModel
+    {
+    }
+}
